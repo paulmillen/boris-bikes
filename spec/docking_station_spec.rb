f873e33158f5c7216964c63e39d115ad0f84c56b
Petra Chocholova <petra.chocholova@gmail.com>
2017-07-06 16:26:39 +0200

    Step 20 completed - doubles for testing are now working

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,27 +3,35 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike}
 
   describe '#release_bike' do
+    let(:bike) { double :bike }
+    it 'releases working bikes' do
+      allow(bike).to receive_messages(
+      :working? => true,
+      :broken? => false)
+      subject.dock(bike)
+      released_bike = subject.release_bike
+      expect(released_bike).to be_working
+    end
+
     it "doesn't give out a bike if there's none" do
       expect { subject.release_bike }.to raise_error "bike not present"
     end
 
     it "doesn't raise the error when we have a bike" do
-      bike = double(:bike)
+      bike = double(:broken? => false)
       subject.dock(bike)
       expect(subject.release_bike).to eq bike
     end
 
     it "doesn't release bike if broken" do
-      bike = double(:bike)
-      bike.report_broken
+      bike = double(:broken? => true)
       subject.dock bike
       expect {subject.release_bike}.to raise_error "Cannot release, bike broken"
     end
 
     it "releases working bikes when one of the docked bikes is broken" do
-      bike1 = double(:bike)
-      bike2 = double(:bike)
-      bike1.report_broken
+      bike1 = double(:broken? => true)
+      bike2 = double(:broken? => false)
       subject.dock bike1
       subject.dock bike2
       expect(subject.release_bike).to eq bike2
