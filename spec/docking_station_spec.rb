4ec3225a31f53a9f33837077d2d1e462f012c087
Petra Chocholova <petra.chocholova@gmail.com>
2017-07-06 14:54:50 +0200

    Step 19 completed - class double used for rspec

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -8,21 +8,21 @@ describe DockingStation do
     end
 
     it "doesn't raise the error when we have a bike" do
-      bike = Bike.new
+      bike = double(:bike)
       subject.dock(bike)
       expect(subject.release_bike).to eq bike
     end
 
     it "doesn't release bike if broken" do
-      bike = Bike.new
+      bike = double(:bike)
       bike.report_broken
       subject.dock bike
       expect {subject.release_bike}.to raise_error "Cannot release, bike broken"
     end
 
     it "releases working bikes when one of the docked bikes is broken" do
-      bike1 = Bike.new
-      bike2 = Bike.new
+      bike1 = double(:bike)
+      bike2 = double(:bike)
       bike1.report_broken
       subject.dock bike1
       subject.dock bike2
@@ -35,7 +35,7 @@ describe DockingStation do
   #it { is_expected.to respond_to(subject.bikes)}
   describe 'initialization' do
     subject { DockingStation.new}
-    let(:bike) { Bike.new }
+    let(:bike) { double(:bike) }
 
     it 'has a vvariable capacity' do
       described_class::DEFAULT_CAPACITY.times do
@@ -47,7 +47,7 @@ describe DockingStation do
   describe '#dock' do
     it 'docks a bike' do
       #bikes = []
-      bike = Bike.new
+      bike = double(:bike)
       bikes = subject.dock(bike)
       expect(subject.dock(bike)).to eq bikes
     end
@@ -56,11 +56,11 @@ describe DockingStation do
 
     end
     it 'can store up to x bikes' do
-      expect( subject.capacity.times { subject.dock(Bike.new)}).to eq subject.capacity
+      expect( subject.capacity.times { subject.dock(double(:bike))}).to eq subject.capacity
     end
 
     it 'returns error when more than x bikes' do
-      expect { ((subject.capacity)+1).times {subject.dock(Bike.new)}}.to raise_error "Docking station full"
+      expect { ((subject.capacity)+1).times {subject.dock(double(:bike))}}.to raise_error "Docking station full"
     end
   end
 end
